f18fe7b79a3fdfd88aaf436111e852f4618f7f21
danielraysa <anel.raysa@gmail.com>
2026-01-29 10:41:17 +0700

    fix middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -38,6 +38,9 @@ return Application::configure(basePath: dirname(__DIR__))
             'signed' => \App\Http\Middleware\ValidateSignature::class,
             'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
             'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
         ]);
 
         $middleware->prepend(\App\Http\Middleware\TrustProxies::class);
@@ -64,7 +67,7 @@ return Application::configure(basePath: dirname(__DIR__))
                     'message' => 'The page expired, please try again.',
                 ]);
             }
-    
+
             return $response;
         });
     })
