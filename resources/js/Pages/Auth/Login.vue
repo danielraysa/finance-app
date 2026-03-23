5286a1a55992a8d042fb84907dc26e3348900b50
danielraysa <anel.raysa@gmail.com>
2026-03-23 22:41:57 +0700

    change logo

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -32,7 +32,9 @@ const submit = () => {
 <template>
     <GuestLayout>
         <Head title="Log in" />
-
+        <div class="mb-4 font-bold text-center text-xl text-gray-800">
+            Aplikasi Keuangan FPPTI JATIM
+        </div>
         <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
             {{ status }}
         </div>
